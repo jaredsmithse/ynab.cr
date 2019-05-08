3bed77a7edf99d701f7cb90efa6b3ade0c92e8db
jaredsmithse <dev@jaredsmith.se>
2019-05-07 20:29:52 -0700

    :fire: Auto-generated tests and comments

--- spec/ynab_spec.cr
+++ spec/ynab_spec.cr
@@ -1,9 +1,5 @@
 require "./spec_helper"
 
 describe YNAB do
-  # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
-  end
 end
--- src/ynab.cr
+++ src/ynab.cr
@@ -1,6 +1,5 @@
-# TODO: Write documentation for `Ynab`
+
 module YNAB
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
 end
